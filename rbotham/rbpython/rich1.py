f4fbea1f6e3fbe90364a8971995d8be57cf93111
Rich Botham <rjbotham@hotmail.com>
2016-12-20 16:09:31 +0000

    From ipad

---
+++ rbotham/rbpython/rich1.py
@@ -0,0 +1,12 @@
+# richs test python script
+
+my_list = ('rich', 'jane', 'charlotte')
+my_slice = my_list[0]
+
+print my_slice[3:]
+
+my_dict = {'name':'rich', 'number':'07885969809'}
+
+print my_dict.keys()
+print my_dict.values()
+
