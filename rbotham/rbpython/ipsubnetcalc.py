93def321f4e729df749a2d086b051682d6583a3e
Rich Botham <rjbotham@hotmail.com>
2016-12-31 16:13:06 +0000

    Text removed to produce a clearer file

--- rbotham/rbpython/ipsubnetcalc.py
+++ rbotham/rbpython/ipsubnetcalc.py
@@ -1,15 +1,6 @@
 # rich test ip subnet calc
 #
 #
-'''
-
-Note on split('0b')[1]
-What this does is format the 'textual' representation of a binary number and removes the '0b' and prints the 1st value in lis t that has been split - NOT the '0'th value.
-This doesnt change the actual value of the real binary number 
-
-note >>> y = ((x ** 2) -2) power of operator
-'''
-
 
 class SubnetCalc(object):
 		def __init__(self, ip_addr, ip_mask):
@@ -37,8 +28,6 @@ class SubnetCalc(object):
 				
 				mask_bits = (32 - hostbits); print "mask bits are ", mask_bits, "\n"
 				
-				
-				#max_hosts = ((2 ** hostbits) -2)
 	
 				'''
 				work out class C
@@ -53,72 +42,8 @@ class SubnetCalc(object):
 			
 				
 							
-										
-																
-				#print "ip address is :" , self.ip_addr , "\n"
-				#print "ip netmask is :" , self.ip_mask , "\n"
-				
-				#print "the bit string is \n\n", bin_mask1, bin_mask2, bin_mask3, bin_mask4, "\n"
-				
-				#print "host bit '0' count is:\t\t", hostbits, "\n"			
-				
-				#print "mask bit '1' count is:\t\t", mask_bits, "\n"
-				
-				#print "max hosts per subnet are:\t" , max_hosts, "\n"
-				
-				#print "max subnets are:\t\t", (max_hosts / 255), "\n"
-				
-				
-				
-					
-					
-											
-
-
-
-
-
 
 #---------------------------------------------------#
-test_ip = SubnetCalc('120.100.45.6', '255.255.255.0')
+test_ip = SubnetCalc('120.100.45.6', '255.255.255.252')
 test_ip.convert_to_binary()
 
-
-'''
-<type 'int'>
-<type 'str'>
->>> 
->>> 
->>> y = ((x ** 2) -2)
-Traceback (most recent call last):
-  File "<string>", line 1, in <module>
-NameError: name 'x' is not defined
->>> y
-Traceback (most recent call last):
-  File "<string>", line 1, in <module>
-NameError: name 'y' is not defined
->>> a = '240'
->>> b = int(a)
->>> c = bin(b)
->>> c
-'0b11110000'
->>> c = bin(b).split(0b)[1]
-  File "<string>", line 1
-    c = bin(b).split(0b)[1]
-                      ^
-SyntaxError: invalid token
->>> c = bin(b)
->>> d = c.split('0b')[1]
->>> d
-'11110000'
->>> e = d.count(0)
-Traceback (most recent call last):
-  File "<string>", line 1, in <module>
-TypeError: expected a character buffer object
->>> e = d.count('0')
->>> e
-4
->>> f = ((2 ** e)-2)
->>> f
-14
-'''
